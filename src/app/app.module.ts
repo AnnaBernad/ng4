ba47d8d553b38438749b4d01eba38b7e72b337cb
AnnaBernad <73069123+AnnaBernad@users.noreply.github.com>
2021-02-12 00:11:53 +0200

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,41 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { UserComponent } from './components/user/user.component';
+import {UserResolveService} from './services/user-resolve.service';
+import {HttpClientModule} from '@angular/common/http';
+import { FullUserComponent } from './components/full-user/full-user.component';
+import { PostComponent } from './components/post/post.component';
+import {PostResolveService} from './services/post-resolve.service';
+import { FullPostComponent } from './components/full-post/full-post.component';
 
+const routes: Routes = [
+  {
+    path: 'users', component: UsersComponent, resolve: {userData: UserResolveService}, children: [
+      {path: ':id', component: FullUserComponent}
+    ]
+  },
+  {path: 'posts', component: PostsComponent, resolve: {postData: PostResolveService}, children: [
+      {path: ':id', component: FullPostComponent}
+    ]}
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    PostsComponent,
+    UserComponent,
+    FullUserComponent,
+    PostComponent,
+    FullPostComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
